6f7dd2737e021c60f3cf81b5ba07e39a795a470c
Michael Schmitz <michael@schmitztech.com>
2013-12-11 15:08:01 -0800

    Add travis.yml.

---
+++ project/Travis.scala
@@ -0,0 +1,4 @@
+credentials += Credentials("Sonatype Nexus Repository Manager",
+                           "54.200.244.75",
+                           "deployment",
+                           "answermyquery")
