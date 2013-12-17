446762a39b64c66e46f79eb1657bac3f415a0d27
Michael Schmitz <michael@schmitztech.com>
2013-12-16 17:00:50 -0800

    Remove extra generic type.

--- src/main/scala/org/allenai/common/Timing.scala
+++ src/main/scala/org/allenai/common/Timing.scala
@@ -20,25 +20,25 @@ object Timing {
     (r, elapsed.nanos)
   }
 
-  def timeNext[R, S](block: =>R)(next: R=>Unit): Duration = {
+  def timeNext[R](block: =>R)(next: R=>Unit): Duration = {
     val (r, duration) = time(block)
     next(r)
     duration
   }
 
-  def timeNextBoth[R, S](block: =>R)(next: (R, Duration)=>Unit): Duration = {
+  def timeNextBoth[R](block: =>R)(next: (R, Duration)=>Unit): Duration = {
     val (r, duration) = time(block)
     next(r, duration)
     duration
   }
 
-  def timeThen[R, S](block: =>R)(handler: Duration=>Unit): R = {
+  def timeThen[R](block: =>R)(handler: Duration=>Unit): R = {
     val (r, duration) = time(block)
     handler(duration)
     r
   }
 
-  def timeThenBoth[R, S](block: =>R)(handler: (R, Duration)=>Unit): R = {
+  def timeThenBoth[R](block: =>R)(handler: (R, Duration)=>Unit): R = {
     val (r, duration) = time(block)
     handler(r, duration)
     r
