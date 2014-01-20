27d4459e3ca7492538627527a3529785a0cb2187
Michael Schmitz <michael@schmitztech.com>
2014-01-20 15:55:27 -0800

    Add log object.

--- src/main/scala/org/allenai/common/Logging.scala
+++ src/main/scala/org/allenai/common/Logging.scala
@@ -11,38 +11,40 @@ import org.slf4j.LoggerFactory
 trait Logging {
   val logger = LoggerFactory.getLogger(this.getClass)
 
-  def trace(message: =>String) =
-    if (logger.isTraceEnabled) {
-      logger.trace(message)
-    }
+  object log {
+    def trace(message: =>String) =
+      if (logger.isTraceEnabled) {
+        logger.trace(message)
+      }
 
-  def debug(message: =>String) =
-    if (logger.isDebugEnabled) {
-      logger.debug(message)
-    }
+    def debug(message: =>String) =
+      if (logger.isDebugEnabled) {
+        logger.debug(message)
+      }
 
-  def info(message: =>String) =
-    if (logger.isInfoEnabled) {
-      logger.info(message)
-    }
+    def info(message: =>String) =
+      if (logger.isInfoEnabled) {
+        logger.info(message)
+      }
 
-  def warn(message: =>String) =
-    if (logger.isWarnEnabled) {
-      logger.warn(message)
-    }
+    def warn(message: =>String) =
+      if (logger.isWarnEnabled) {
+        logger.warn(message)
+      }
 
-  def warn(message: =>String, throwable: Throwable) =
-    if (logger.isWarnEnabled) {
-      logger.warn(message, throwable)
-    }
+    def warn(message: =>String, throwable: Throwable) =
+      if (logger.isWarnEnabled) {
+        logger.warn(message, throwable)
+      }
 
-  def error(message: =>String) =
-    if (logger.isErrorEnabled) {
-      logger.error(message)
-    }
+    def error(message: =>String) =
+      if (logger.isErrorEnabled) {
+        logger.error(message)
+      }
 
-  def error(message: =>String, throwable: Throwable) =
-    if (logger.isErrorEnabled) {
-      logger.error(message, throwable)
-    }
+    def error(message: =>String, throwable: Throwable) =
+      if (logger.isErrorEnabled) {
+        logger.error(message, throwable)
+      }
+  }
 }
