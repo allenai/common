6c7ec74cd5f44aad101d9ff8a7c29304e9461a81
Michael Schmitz <michael@schmitztech.com>
2014-06-23 16:01:43 -0700

    Travis test.

--- project/Build.scala
+++ project/Build.scala
@@ -1,7 +1,6 @@
 import sbt._
 import Keys._
 
-object CommonBuild extends Build {
   val inheritedSettings = Defaults.defaultSettings ++ Format.settings ++ Publish.settings ++
     TravisPublisher.settings
 
