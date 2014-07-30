bf0de209fe4da417a0f1db77bbc4e1d8357414e5
Michael Schmitz <michael@schmitztech.com>
2014-07-30 10:28:15 -0700

    Changes per code review.

--- core/src/main/scala/org/allenai/common/Analysis.scala
+++ core/src/main/scala/org/allenai/common/Analysis.scala
@@ -8,9 +8,7 @@ object Analysis {
     *
     * @return  a yield, precision point
     */
-  def areaUnderCurve(points: Seq[(Int, Double)]) = {
-    val max = points.iterator.map(_._1).max
-
+  def areaUnderCurve(points: Seq[(Int, Double)]): Double = {
     val it = points.iterator.buffered
     var cur = (0, 1.0)
     var area = 0.0
@@ -31,19 +29,21 @@ object Analysis {
   /** Compute precision yield points for each yield value.
     * Scores should be ordered by confidence, descending.
     *
-    * @return  a yield, precision point
+    * @return  a sequence of (yield, precision) points
     */
   def precisionYield(scores: Seq[Boolean]): Seq[(Int, Double)] = {
     var correct = 0
     var incorrect = 0
-    var points = List[(Int, Double)]()
+    var points = List.empty[(Int, Double)]
 
     for (score <- scores) {
-      if (score) correct = correct + 1
-      else incorrect = incorrect + 1
-
       if (score) {
-        points ::= (correct, precision(correct, incorrect))
+        correct = correct + 1
+
+        val point = (correct, precision(correct, incorrect))
+        points ::= point
+      } else {
+        incorrect = incorrect + 1
       }
     }
 
@@ -51,28 +51,32 @@ object Analysis {
   }
 
   /** Compute precision yield points for each change in the first part of the tuple.
-    * For example, this method if the first part of the tuple is a confidence value we would have precision yield points for each change in confidence.
+    * For example, if the first part of the tuple is a confidence value we would have
+    * precision yield points for each change in confidence.
     *
     * Scored examples are presumed to be in sorted order by T, descending.
     *
     * @return  a triple of T, the yield, and the precision
     */
   def precisionYieldMeta[T](scores: Seq[(T, Boolean)]): Seq[(T, Int, Double)] = {
-    if (scores.length == 0) List()
-    else {
+    if (scores.length == 0) {
+      List()
+    } else {
       var correct = 0
       var incorrect = 0
       var points = List[(T, Int, Double)]()
-      var last = scores.head._1
+      var previous = scores.head._1
 
       var i = 0
       for ((meta, score) <- scores) {
         if (score) correct = correct + 1
         else incorrect = incorrect + 1
 
-        if (meta != last || i == scores.length - 1) {
-          last = meta
-          points ::= (meta, correct, precision(correct, incorrect))
+        if (meta != previous || i == scores.length - 1) {
+          previous = meta
+
+          val point = (meta, correct, precision(correct, incorrect))
+          points ::= point
         }
 
         i = i + 1
@@ -84,12 +88,18 @@ object Analysis {
 
   /** Compute precision from counts of correct and incorrect examples.
     */
-  def precision(correct: Int, incorrect: Int): Double =
+  def precision(correct: Int, incorrect: Int): Double = {
+    require(correct >= 0 && incorrect >= 0, "arguments must not be negative.")
+    require(correct + incorrect > 0, "There must be at least one example.")
+
     correct.toDouble / (correct + incorrect).toDouble
+  }
 
   /** Compute precision from a series of evaluations.
     */
   def precision(scores: Seq[Boolean]): Double = {
+    require(!scores.isEmpty, "scores cannot be empty")
+
     val correct = scores.count(_ == true)
     precision(correct, scores.size - correct)
   }
