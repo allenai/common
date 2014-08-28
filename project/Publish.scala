b82c420adc155c45590a4a03d3994a4da9352188
Dirk Groeneveld <dirkg@allenai.org>
2014-08-28 13:22:18 -0700

    Changing where we publish, removing old travis stuff.

--- project/Publish.scala
+++ project/Publish.scala
@@ -6,17 +6,11 @@ import scala.util.Try
 /** Publish settings, for a public repository. */
 object Publish {
   lazy val settings = Seq(
-    credentials += Credentials("Sonatype Nexus Repository Manager",
-                               "oss.sonatype.org",
-                               "allenai-role",
-                               System.getenv("NEXUS_PASS")),
     publishTo <<= isSnapshot { isSnap =>
       if (isSnap) {
         Some("snapshots" at "https://oss.sonatype.org/content/repositories/snapshots")
-      } else if (Try(sys.env("TRAVIS")).getOrElse("false") == "true") {
-        None
       } else {
-        Some("releases" at "https://oss.sonatype.org/service/local/staging/deploy/maven2")
+        Some("releases" at "https://oss.sonatype.org/content/repositories/releases")
       }
     },
     licenses := Seq(
