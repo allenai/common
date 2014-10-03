4bbf6a09547a2e44cc82ae3f86878abd0ff11644
Dirk Groeneveld <dirkg@allenai.org>
2014-10-03 16:59:26 -0700

    Added comment for LocatorConfig

--- datastore-cli/src/main/scala/org/allenai/datastore/cli/LocatorConfig.scala
+++ datastore-cli/src/main/scala/org/allenai/datastore/cli/LocatorConfig.scala
@@ -2,6 +2,9 @@ package org.allenai.datastore.cli
 
 import org.allenai.datastore.Datastore
 
+/** A lot of the configs we're using contain information to make a Datastore Locator object. This
+  * trait puts that idea into one place.
+  */
 trait LocatorConfig {
   val group: String
   val name: String
