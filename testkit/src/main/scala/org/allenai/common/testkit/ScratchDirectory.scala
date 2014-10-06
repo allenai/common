74c1e5e6f25b1c86044e3b89e062bc51d79edb2d
rodneykinney <rodneyk@allenai.org>
2014-10-06 15:04:59 -0700

    Use Files.createTempDirectory

--- testkit/src/main/scala/org/allenai/common/testkit/ScratchDirectory.scala
+++ testkit/src/main/scala/org/allenai/common/testkit/ScratchDirectory.scala
@@ -3,36 +3,28 @@ package org.allenai.common.testkit
 import org.scalatest.BeforeAndAfterAll
 
 import java.io.File
+import java.nio.file.Files
 
 /** Provides a scratch directory for writing unit-test output */
 trait ScratchDirectory extends BeforeAndAfterAll {
   this: UnitSpec =>
 
-  val scratchDir: File = createScratchDirectory()
-
-  def createScratchDirectory(): File = {
-    val dir = File.createTempFile(this.getClass.getSimpleName, "scratch")
-    if (dir.exists && !dir.isDirectory) {
-      dir.delete()
-    }
-    if (!dir.exists) {
-      dir.mkdirs()
-    }
+  val scratchDir: File = {
+    val dir = Files.createTempDirectory(this.getClass.getSimpleName).toFile
+    sys.addShutdownHook(delete(dir))
     dir
   }
 
-  override def beforeAll: Unit = {
-    require(scratchDir.exists && scratchDir.isDirectory,
-      s"Unable to create scratch directory $scratchDir")
+  override def beforeAll: Unit = require(scratchDir.exists && scratchDir.isDirectory,
+    s"Unable to create scratch directory $scratchDir")
+
+  override def afterAll: Unit = delete(scratchDir)
+
+  private def delete(f: File) {
+    if (f.isDirectory()) {
+      f.listFiles.foreach(delete)
+    }
+    f.delete()
   }
 
-  override def afterAll: Unit = {
-    def delete(f: File) {
-      if (f.isDirectory()) {
-        f.listFiles.foreach(delete)
-      }
-      f.delete()
-    }
-    delete(scratchDir)
-  }
 }
