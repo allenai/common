aa1cb64eeb53d84e48db79cbcdc410d1ad42d080
Dirk Groeneveld <dirkg@allenai.org>
2014-10-06 17:16:42 -0700

    Giving up on common config parsing between upload and download app Download app complete

--- datastore-cli/src/main/scala/org/allenai/datastore/cli/DownloadApp.scala
+++ datastore-cli/src/main/scala/org/allenai/datastore/cli/DownloadApp.scala
@@ -1,5 +1,66 @@
 package org.allenai.datastore.cli
 
-object DownloadApp extends App {
+import org.allenai.datastore.Datastore
 
+object DownloadApp extends App {
+  case class Config(
+    assumeFile: Boolean = false,
+    assumeDirectory: Boolean = false,
+    group: String = null,
+    name: String = null,
+    version: Int = -1,
+    datastore: Datastore = Datastore)
+
+  val parser = new scopt.OptionParser[Config]("scopt") {
+    opt[Boolean]('f', "assumeFile") action { (f, c) =>
+      c.copy(assumeFile = f)
+    } text ("Assumes that the object in the datastore is a file.")
+
+    opt[Boolean]('d', "assumeDirectory") action { (d, c) =>
+      c.copy(assumeDirectory = d)
+    } text ("Assumes that the object in the datastore is a directory.")
+
+    checkConfig { c =>
+      if (c.assumeDirectory && c.assumeFile) {
+        failure("You can't specify both assumeDirectory and assumeFile")
+      } else {
+        success
+      }
+    }
+
+    note("If you specify neither assumeDirectory nor assumeFile, the tool will autodetect whether " +
+      "object in the datastore is a file or a directory.")
+
+    opt[String]('g', "group") required () action { (g, c) =>
+      c.copy(group = g)
+    } text ("Group name of the object in the datastore")
+
+    opt[String]('n', "name") required () action { (n, c) =>
+      c.copy(name = n)
+    } text ("Name of the object in the datastore")
+
+    opt[Int]('v', "version") required () action { (v, c) =>
+      c.copy(version = v)
+    } text ("Version number of the object in the datastore")
+
+    opt[String]('d', "datastore") action { (d, c) =>
+      c.copy(datastore = new Datastore(d))
+    } text (s"Datastore to use. Default is ${Datastore.name}")
+
+    help("help")
+  }
+
+  parser.parse(args, Config()) foreach { config =>
+    val datastore = config.datastore
+    val locator = datastore.Locator(config.group, config.name, config.version)
+    val fileMode = if (config.assumeDirectory) {
+      false
+    } else if (config.assumeFile) {
+      true
+    } else {
+      !datastore.directoryExists(locator)
+    }
+
+    println(if (fileMode) datastore.filePath(locator) else datastore.directoryPath(locator))
+  }
 }
--- datastore-cli/src/main/scala/org/allenai/datastore/cli/UploadApp.scala
+++ datastore-cli/src/main/scala/org/allenai/datastore/cli/UploadApp.scala
@@ -7,11 +7,11 @@ import java.io.File
 object UploadApp extends App {
   case class Config(
     path: File = null,
-    override val group: String = null,
-    override val name: String = null,
-    override val version: Int = -1,
-    override val datastore: Datastore = Datastore,
-    overwrite: Boolean = false) extends LocatorConfig
+    group: String = null,
+    name: String = null,
+    version: Int = -1,
+    datastore: Datastore = Datastore,
+    overwrite: Boolean = false)
 
   val parser = new scopt.OptionParser[Config]("scopt") {
     opt[File]('p', "path") required () action { (p, c) =>
@@ -32,7 +32,7 @@ object UploadApp extends App {
 
     opt[String]('d', "datastore") action { (d, c) =>
       c.copy(datastore = new Datastore(d))
-    } text ("Datastore to use")
+    } text (s"Datastore to use. Default is ${Datastore.name}")
 
     opt[Boolean]("overwrite") action { (_, c) =>
       c.copy(overwrite = true)
@@ -42,15 +42,16 @@ object UploadApp extends App {
   }
 
   parser.parse(args, Config()) foreach { config =>
+    val locator = config.datastore.Locator(config.group, config.name, config.version)
     if (config.path.isDirectory) {
       config.datastore.publishDirectory(
         config.path.toPath,
-        config.locator,
+        locator,
         config.overwrite)
     } else {
       config.datastore.publishFile(
         config.path.toPath,
-        config.locator,
+        locator,
         config.overwrite)
     }
   }
