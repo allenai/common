a9fc1d3565027f630f58c291cb7ee3605e999f7e
Dirk Groeneveld <dirkg@allenai.org>
2014-11-03 16:50:24 -0800

    Removes the datastore, since it's been moved to its own project

--- project/Build.scala
+++ project/Build.scala
@@ -44,23 +44,8 @@ object CommonBuild extends Build {
     settings = buildSettings
   ).dependsOn(testkit % "test->compile", core).enablePlugins(AllenaiReleasePlugin)
 
-  lazy val datastore = Project(
-    id = "datastore",
-    base = file("datastore"),
-    settings = buildSettings ++ Defaults.itSettings
-  ).dependsOn(core, testkit % "test,it").
-    enablePlugins(AllenaiReleasePlugin).
-    configs(IntegrationTest)
-
-  lazy val datastoreCli = Project(
-    id = "datastore-cli",
-    base = file("datastore-cli"),
-    settings = buildSettings
-  ).dependsOn(datastore).
-    enablePlugins(AllenaiReleasePlugin)
-
   lazy val common = Project(id = "common", base = file(".")).settings(
     // Don't publish a jar for the root project.
     publishTo := Some("dummy" at "nowhere"), publish := { }, publishLocal := { }
-  ).aggregate(webapp, core, testkit, pipeline, datastore, datastoreCli).enablePlugins(AllenaiReleasePlugin)
+  ).aggregate(webapp, core, testkit, pipeline).enablePlugins(AllenaiReleasePlugin)
 }
