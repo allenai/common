bbed5b3a22ddf97f71ae8a5c7959d3e7f7471f98
Dirk Groeneveld <dirkg@allenai.org>
2014-11-12 14:48:21 -0800

    Update to latest plugin version

--- project/Build.scala
+++ project/Build.scala
@@ -1,5 +1,5 @@
-import org.allenai.sbt.release.AllenaiReleasePlugin
-import org.allenai.sbt.core.CoreSettings
+import org.allenai.plugins.ReleasePlugin
+import org.allenai.plugins.CoreRepositories
 import sbtrelease.ReleasePlugin._
 
 import sbt._
@@ -17,34 +17,34 @@ object CommonBuild extends Build {
       "Apache 2.0" -> new URL("http://www.apache.org/licenses/LICENSE-2.0.txt")
     )
   ) ++ 
-    CoreSettings.publishToRepos.ai2.publicRepo ++
+    CoreRepositories.PublishTo.ai2Public ++
     releaseSettings
 
   lazy val testkit = Project(
     id = "testkit",
     base = file("testkit"),
-    settings = buildSettings).enablePlugins(AllenaiReleasePlugin)
+    settings = buildSettings).enablePlugins(ReleasePlugin)
 
   lazy val core = Project(
     id = "core",
     base = file("core"),
     settings = buildSettings
-  ).enablePlugins(AllenaiReleasePlugin).dependsOn(testkit % "test->compile")
+  ).enablePlugins(ReleasePlugin).dependsOn(testkit % "test->compile")
 
   lazy val webapp = Project(
     id = "webapp",
     base = file("webapp"),
     settings = buildSettings
-  ).enablePlugins(AllenaiReleasePlugin).dependsOn(core)
+  ).enablePlugins(ReleasePlugin).dependsOn(core)
 
   lazy val pipeline = Project(
     id = "pipeline",
     base = file("pipeline"),
     settings = buildSettings
-  ).dependsOn(testkit % "test->compile", core).enablePlugins(AllenaiReleasePlugin)
+  ).dependsOn(testkit % "test->compile", core).enablePlugins(ReleasePlugin)
 
   lazy val common = Project(id = "common", base = file(".")).settings(
     // Don't publish a jar for the root project.
     publishTo := Some("dummy" at "nowhere"), publish := { }, publishLocal := { }
-  ).aggregate(webapp, core, testkit, pipeline).enablePlugins(AllenaiReleasePlugin)
+  ).aggregate(webapp, core, testkit, pipeline).enablePlugins(ReleasePlugin)
 }
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -1,7 +1,3 @@
-lazy val ai2PluginsVersion = "2014.11.03-0"
-
-addSbtPlugin("org.allenai.plugins" % "allenai-sbt-core-settings" % ai2PluginsVersion)
-
-addSbtPlugin("org.allenai.plugins" % "allenai-sbt-release" % ai2PluginsVersion)
+addSbtPlugin("org.allenai.plugins" % "allenai-sbt-plugins" % "2014.11.11-0")
 
 addSbtPlugin("com.eed3si9n" % "sbt-assembly" % "0.11.2")
