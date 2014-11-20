2ce5f64cb6b701f10ff8fd9f5db7264eaae9657e
Michael Schmitz <michael@schmitztech.com>
2014-11-20 12:33:54 -0800

    Use Scala 2.11.

--- project/Build.scala
+++ project/Build.scala
@@ -9,11 +9,10 @@ import Keys._
 object CommonBuild extends Build {
   val buildSettings = releaseSettings ++ Seq(
     organization := "org.allenai.common",
-    crossScalaVersions := Seq("2.10.4"),
+    crossScalaVersions := Seq("2.11.4"),
     scalaVersion <<= crossScalaVersions { (vs: Seq[String]) => vs.head },
     scalacOptions ++= Seq("-Xlint", "-deprecation", "-unchecked", "-feature"),
     conflictManager := ConflictManager.strict,
-    dependencyOverrides ++= Dependencies.Overrides,
     publishMavenStyle := true,
     publishArtifact in Test := false,
     pomIncludeRepository := { _ => false },
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -1,4 +1,4 @@
-addSbtPlugin("org.allenai.plugins" % "allenai-sbt-plugins" % "2014.11.11-0")
+addSbtPlugin("org.allenai.plugins" % "allenai-sbt-plugins" % "2014.11.20-0-SNAPSHOT")
 
 addSbtPlugin("com.jsuereth" % "sbt-pgp" % "1.0.0")
 
