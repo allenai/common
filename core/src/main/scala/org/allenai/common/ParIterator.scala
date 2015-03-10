1809f4bdb79790fc14d907330090ec9f40ab4635
Dirk Groeneveld <dirkg@allenai.org>
2015-03-09 18:12:41 -0700

    JavaDoc

--- core/src/main/scala/org/allenai/common/ParIterator.scala
+++ core/src/main/scala/org/allenai/common/ParIterator.scala
@@ -9,10 +9,23 @@ import scala.util.control.NonFatal
 
 object ParIterator {
   val queueLimit = 1024
-  val exceptionLimit = 1024
 
   implicit class ParIteratorEnrichment[T](val input: Iterator[T]) extends AnyVal {
 
+    /** Runs the given function over all values from the iterator in parallel, and returns when they
+      * are all done.
+      *
+      * The maximum parallelism that's supported by the execution context applies. Also, this
+      * function takes care not to overload the execution context with more requests than it can
+      * handle.
+      *
+      * If one or more of the function executions throw an exception, only the first exception is
+      * reported. The iterator is still exhausted, and all other executions are attempted. That's
+      * impractical, but consistent with the behavior of the parallel collections in Scala.
+      *
+      * @param f  the function to execute
+      * @param ec the execution context to run the function executions in
+      */
     def parForeach(f: T => Unit)(implicit ec: ExecutionContext): Unit = {
       // If there are a billion items in the iterator, we don't want to create a billion futures,
       // so we limit the number of futures we create with this semaphore.
@@ -65,6 +78,22 @@ object ParIterator {
       firstException.get().foreach { case (_, e) => throw e }
     }
 
+    /** Maps an iterator to another iterator, performing the maps on the elements in parallel.
+      * Returns an iterator with the values mapped by the given function.
+      *
+      * The maximum parallelism that's supported by the execution context applies. Also, this
+      * function makes sure to not overwhelm the JVM's resources with too many temporary results. To
+      * do this, the returned iterator will "work ahead" of the thread that's reading from it, but
+      * there is a limit to how far ahead it will work.
+      *
+      * If one or more of the map function executions throw an exception, only the first exception is
+      * reported, and it is reported when you call next() on the returned iterator.
+      *
+      * @param f  the function performing the mapping
+      * @param ec the execution context to run the function executions in
+      * @tparam O the type of the output
+      * @return   a new iterator with the mapped values from the old iterator
+      */
     def parMap[O](f: T => O)(implicit ec: ExecutionContext): TraversableOnce[O] = new Iterator[O] {
       private val inner = input.toIterator
       private val q = new scala.collection.mutable.Queue[Future[O]]()
