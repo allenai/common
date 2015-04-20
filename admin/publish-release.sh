a093cc756c06fb6131daa3902dc00836ce25fd29
Michael Schmitz <michael@schmitztech.com>
2015-04-20 16:01:06 -0700

    Add better messaging.

--- admin/publish-release.sh
+++ admin/publish-release.sh
@@ -1,13 +1,25 @@
 #!/bin/bash
 
+set -e
+
 # Publish to BinTray if the HEAD commit is tagged with a version number.
+
+echo $PULL_REQUEST
+
+if [[ $PULL_REQUEST ]]; then
+  echo "Shippable is building a pull request, not publishing."
+  exit 0
+fi
 
 headTag=$(git describe --exact-match ||:)
 echo "Current tag is: $headTag"
 
-if [[ headTag =~ ^v[0-9]+\.[0-9]+\.[0-9]+(-[A-Za-z0-9-]+)? ]]; then
+if [[ ! PULL_REQUEST && headTag =~ ^v[0-9]+\.[0-9]+\.[0-9]+(-[A-Za-z0-9-]+)? ]]; then
   echo "Going to release from tag $headTag."
   version=$(echo $headTag | sed -e s/^v//)
 
-  sbt publish
+  echo sbt publish
+  echo "Successfully published artifact."
+
+  exit 0
 fi
