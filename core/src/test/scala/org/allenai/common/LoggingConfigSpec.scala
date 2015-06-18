989b836178663f5dd19930f058f621a3f8c9f397
cristipp@allenai.org <cristipp@allenai.org>
2015-06-18 15:13:15 -0700

    fixed test

--- core/src/test/scala/org/allenai/common/LoggingConfigSpec.scala
+++ core/src/test/scala/org/allenai/common/LoggingConfigSpec.scala
@@ -15,21 +15,20 @@ class LoggingConfigSpec extends UnitSpec with Logging {
     val l = logger.config("org.allenai.common")
     l.reset()
     l.addAppender(
-      logger.factory.patternLayoutEncoder("%-5level [%thread]: %message%n"),
+      logger.factory.patternLayoutEncoder("%-5level: %message%n"),
       logger.factory.fileAppender(path.toString)
     )
     l.setLevel(Level.WARN)
+
     logger.info("info should not be visible")
     logger.warn("warn should be visible")
     logger.warn("warn should be visible 2")
-
+    
     assert(
       Source.fromFile(path.toString).mkString ===
-        """WARN  [ScalaTest-run-running-LoggingConfigSpec]: warn should be visible
-        |WARN  [ScalaTest-run-running-LoggingConfigSpec]: warn should be visible 2
-        |""".stripMargin
+        """WARN : warn should be visible
+          |WARN : warn should be visible 2
+          |""".stripMargin
     )
-
-    println(Source.fromFile(path.toString).mkString)
   }
 }
