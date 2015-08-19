811c2de6eaea73550cd597ebbc602a9efc73c134
Ryan Benmalek <ryanai3@hotmail.com>
2015-08-19 13:32:54 -0700

    test bug - spray has issues deserializing case class containing case class containing Option(String) - replaced with vanilla case class serialization/deserialization test

--- core/src/test/scala/org/allenai/common/cache/QueryCacheSpec.scala
+++ core/src/test/scala/org/allenai/common/cache/QueryCacheSpec.scala
@@ -3,15 +3,24 @@ package org.allenai.common.cache
 import org.allenai.common.{ GitVersion, Version, cache }
 import org.allenai.common.testkit.UnitSpec
 import org.scalatest.BeforeAndAfterAll
-import sys.process._
+import spray.json.DefaultJsonProtocol
 import spray.json.DefaultJsonProtocol._
+import spray.json._
+import scala.sys.process
+
+case class Foo(stringVar: String, intVar: Int)
+
+object FooJsonProtocol extends DefaultJsonProtocol {
+  implicit val fooFormat = jsonFormat2(Foo)
+}
 
 class QueryCaches(redisHostname: String, redisPort: Int) {
   val stringQueryCache = new JsonQueryCache[String](redisHostname, redisPort, "test")
   val intQueryCache = new JsonQueryCache[Int](redisHostname, redisPort, "test")
   val seqStringQueryCache = new JsonQueryCache[Seq[String]](redisHostname, redisPort, "test")
   // It's an object I can test
-  val versionQueryCache = new JsonQueryCache[Version](redisHostname, redisPort, "test")
+  import FooJsonProtocol._
+  val versionQueryCache = new JsonQueryCache[Foo](redisHostname, redisPort, "test")
 
   val stringKey = "stringKey"
   val stringValue = "stringValue"
@@ -22,35 +31,35 @@ class QueryCaches(redisHostname: String, redisPort: Int) {
   val seqStringKey = "seqStringKey"
   val seqStringValue = Seq("a string", "a second string", "third time's the charm")
 
-  val versionKey = "versionKey"
-  val versionValue = new Version(new GitVersion("test", 324, Option("tester")), "artifact")
+  val fooKey = "fooKey"
+  val fooValue = new Foo("stringerino", 42)
 
   def getAll(): Seq[Option[Any]] = Seq(
     stringQueryCache.get(stringKey),
     intQueryCache.get(intKey),
     seqStringQueryCache.get(seqStringKey),
-    versionQueryCache.get(versionKey)
+    fooQueryCache.get(versionKey)
   )
 
   def putAll(): Unit = {
     stringQueryCache.put(stringKey, stringValue)
     intQueryCache.put(intKey, intValue)
     seqStringQueryCache.put(seqStringKey, seqStringValue)
-    versionQueryCache.put(versionKey, versionValue)
+    fooQueryCache.put(versionKey, versionValue)
   }
 
   def delAll(): Unit = {
     stringQueryCache.del(stringKey)
     intQueryCache.del(intKey)
     seqStringQueryCache.del(seqStringKey)
-    versionQueryCache.del(versionKey)
+    fooQueryCache.del(versionKey)
   }
 
   def allThereAndEq(): Boolean = {
-    stringQueryCache.get(stringKey).exists(_.equals(stringValue))
-    intQueryCache.get(intKey).exists(_.equals(intValue))
-    seqStringQueryCache.get(seqStringKey).exists(_.equals(seqStringValue))
-    versionQueryCache.get(versionKey).exists(_.equals(versionValue))
+    stringQueryCache.get(stringKey).exists(_.equals(stringValue)) &&
+      intQueryCache.get(intKey).exists(_.equals(intValue)) &&
+      seqStringQueryCache.get(seqStringKey).exists(_.equals(seqStringValue)) &&
+      fooQueryCache.get(fooKey).exists(_.equals(fooValue))
   }
 }
 
