5c7c1b192b2088fcc8698604fff3e51c6851ebc0
Jesse Kinkead <jessek@allenai.org>
2016-01-28 11:21:42 -0800

    Add a new constructor method that loads from a specified class loader.
    Add a unit test for this, and clean up some docs.

--- core/src/test/scala/org/allenai/common/VersionSpec.scala
+++ core/src/test/scala/org/allenai/common/VersionSpec.scala
@@ -6,37 +6,67 @@ import org.allenai.common.testkit.UnitSpec
 import spray.json._
 import spray.json.DefaultJsonProtocol._
 
+import java.net.URLClassLoader
+import java.nio.file.Paths
+
 import scala.util.{ Try, Success, Failure }
 
 class GitVersionSpec extends UnitSpec {
-  "GitVersion" should "find the correct GitHub project URL (ssh)" in {
+  "create" should "find the correct GitHub project URL (ssh)" in {
     val version = GitVersion.create("gitSha", 1234, Seq(
       "https://github.com/schmmd/parsers.git",
       "git@github.com:allenai/common.git"
     ))
-    assert(version.repoUrl === Some("http://github.com/allenai/common"))
+    version.repoUrl shouldBe Some("http://github.com/allenai/common")
   }
 
-  "GitVersion" should "find the correct GitHub project URL (https)" in {
+  it should "find the correct GitHub project URL (https)" in {
     val version = GitVersion.create("gitSha", 1234, Seq(
       "https://github.com/allenai/ari-datastore.git",
       "git@github.com:schmmd/common.git"
     ))
-    assert(version.repoUrl === Some("http://github.com/allenai/ari-datastore"))
+    version.repoUrl shouldBe Some("http://github.com/allenai/ari-datastore")
   }
 
-  "GitVersion" should "find the correct GitHub commit URL" in {
+  it should "find the correct GitHub commit URL" in {
     val version = GitVersion.create("e0d972e185bd12b94dedd38834fea150a68f064e", 1234,
       Seq("https://github.com/allenai/parsers.git", "git@github.com:schmmd/common.git"))
-    assert(version.commitUrl ===
-      Some("http://github.com/allenai/parsers/commit/e0d972e185bd12b94dedd38834fea150a68f064e"))
+    version.commitUrl shouldBe
+      Some("http://github.com/allenai/parsers/commit/e0d972e185bd12b94dedd38834fea150a68f064e")
   }
 }
 
 class VersionSpec extends UnitSpec {
   "Version" should "be backwards compatible for reading" in {
-    val json = """{  "git":"0144af4325992689cf5fd6d0e3c2d744b25935d6","artifact":"2014.07.21-0-SNAPSHOT","commitDate":1412094251000}"""
-    assert(json.parseJson.convertTo[Version] ===
-      Version(GitVersion("0144af4325992689cf5fd6d0e3c2d744b25935d6", 1412094251000L, None), "2014.07.21-0-SNAPSHOT", None))
+    val json = """{
+      "git":"0144af4325992689cf5fd6d0e3c2d744b25935d6",
+      "artifact":"2014.07.21-0-SNAPSHOT","commitDate":1412094251000
+    }"""
+    json.parseJson.convertTo[Version] shouldBe
+      Version(
+        GitVersion("0144af4325992689cf5fd6d0e3c2d744b25935d6", 1412094251000L, None),
+        "2014.07.21-0-SNAPSHOT",
+        None
+      )
+  }
+
+  "fromResources" should "find common-core's resources" in {
+    val version = Version.fromResources("org.allenai.common", "common-core")
+    // No asserts; this will throw an exception if it's unfound.
+  }
+
+  it should "find a resource using a class loader" in {
+    val expectedVersion = Version(
+      GitVersion("sha123", 123456789L, None),
+      "1.0.0",
+      None
+    )
+    val classpath = Paths.get("src/test/resources/fakejar").toAbsolutePath.toUri.toURL
+    val version = Version.fromResources(
+      "org.fakeorg",
+      "project-name",
+      new URLClassLoader(Array(classpath))
+    )
+    version shouldBe expectedVersion
   }
 }
