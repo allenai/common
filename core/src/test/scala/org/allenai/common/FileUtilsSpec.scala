0a33f217d72d487e3c9dcbdec114ea46e2155db4
Ashish Sabharwal <AshishS@allenai.org>
2016-04-27 16:57:06 -0700

    minor string edit

--- core/src/test/scala/org/allenai/common/FileUtilsSpec.scala
+++ core/src/test/scala/org/allenai/common/FileUtilsSpec.scala
@@ -4,13 +4,13 @@ import org.allenai.common.testkit.UnitSpec
 
 class FileUtilsSpec extends UnitSpec {
 
-  "FileUtils" should "correctly read plain text file" in {
+  "FileUtils" should "correctly read plain text resource" in {
     val lines = FileUtils.getResourceAsLines("testfile.txt", getClass)
     val whatIsExpected = Seq("This is line 1.", "This is line 2.", "...", "This is line N.")
     lines should be(whatIsExpected)
   }
 
-  it should "correctly read CSV file" in {
+  it should "correctly read CSV resource" in {
     val lines = FileUtils.getCSVContentFromResource("testfile.txt", getClass)
     val whatIsExpected = Seq(Seq("a1", "b1", "c1"), Seq("a2", "b2", "c2"), Seq("a3", "b3", "c3"))
     lines should be(whatIsExpected)
