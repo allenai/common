3435d46aea091013a2aa3b4f3bc8f8332252f1d9
Joel Grus <joelgrus@gmail.com>
2016-11-14 16:26:30 -0800

    update BingClient to bing v5 api (#212)
    * update bing client to v5
    
    * add count parameter
    
    * sbt format

--- webapp/src/test/scala/org/allenai/common/webapp/BingClientSpec.scala
+++ webapp/src/test/scala/org/allenai/common/webapp/BingClientSpec.scala
@@ -6,7 +6,19 @@ import java.util.NoSuchElementException
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.util.{ Failure, Success }
 
+import java.net.URISyntaxException
+
 class BingClientSpec extends UnitSpec {
+  // Not perfect, I got it from http://stackoverflow.com/a/20039133/1076346
+  def isValidUri(maybeUri: String): Boolean = {
+    try {
+      val uri = new java.net.URI(maybeUri)
+      uri.getHost() != null
+    } catch {
+      case e: URISyntaxException => false
+    }
+  }
+
   // If the Azure auth key isn't defined, just skip these tests.
   val apiKey = try {
     Some(sys.env("AZURE_AUTH_KEY"))
@@ -20,6 +32,7 @@ class BingClientSpec extends UnitSpec {
     if (bingClient.isDefined) {
       val results = bingClient.get.query("aardvark")
       assert(results.nonEmpty)
+      assert(results.map(_.url).forall(isValidUri))
     } else {
       cancel("AZURE_AUTH_KEY not defined, skipping test")
     }
