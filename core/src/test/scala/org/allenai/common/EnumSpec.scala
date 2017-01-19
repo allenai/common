a1e4ba4aa7f5f33443f236308f6648a071d5f0af
Jesse Kinkead <jessek@allenai.org>
2017-01-18 21:58:26 -0800

    Restore serialization when a constructor argument exists.
    Add a unit test to cover this case.

--- core/src/test/scala/org/allenai/common/EnumSpec.scala
+++ core/src/test/scala/org/allenai/common/EnumSpec.scala
@@ -40,8 +40,8 @@ class EnumSpec extends UnitSpec {
     }
   }
 
-  "Java serialization" should "work" in {
-    FakeEnum.all foreach { enum =>
+  it should "work with a constructor argument" in {
+    FakeEnumWithId.all foreach { enum =>
       val tmp = Files.createTempFile(enum.id, "dat")
       val tmpFile = tmp.toFile()
       tmpFile.deleteOnExit()
@@ -64,5 +64,15 @@ object FakeEnum extends EnumCompanion[FakeEnum] {
   case object Value1 extends FakeEnum
   case object Value2 extends FakeEnum
   case object Value3 extends FakeEnum
+  register(Value1, Value2, Value3)
+}
+
+// Test enum. Must be defined outside of spec otherwise serialization tests will
+// fail due to scalatest WordSpec not being serializable.
+sealed abstract class FakeEnumWithId(override val id: String) extends Enum[FakeEnumWithId]
+object FakeEnumWithId extends EnumCompanion[FakeEnumWithId] {
+  case object Value1 extends FakeEnumWithId("one")
+  case object Value2 extends FakeEnumWithId("two")
+  case object Value3 extends FakeEnumWithId("three")
   register(Value1, Value2, Value3)
 }
