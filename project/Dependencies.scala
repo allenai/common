1447581b2d7e776a7e0396fb79d96eada27f7724
Russell Reas <russellr@allenai.org>
2019-09-10 13:34:24 -0700

    Copy Dependencies and update minimally where required for 2.12

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -10,9 +10,6 @@ object Dependencies {
 
   val commonsIO = "commons-io" % "commons-io" % "2.4"
 
-  // TODO(jkinkead) Break circular dependency between common and datastore
-  val datastore = "org.allenai" %% "datastore" % "1.0.0"
-
   val elasticSearch = "org.elasticsearch" % "elasticsearch" % "2.3.3"
 
   val jedis = "redis.clients" % "jedis" % "2.7.2"
@@ -28,41 +25,32 @@ object Dependencies {
 
   val pegdown = "org.pegdown" % "pegdown" % "1.4.2"
 
-  val scalaGuice = "net.codingwell" %% "scala-guice" % "4.0.1"
+  val scalaGuice = "net.codingwell" %% "scala-guice" % "4.2.6"
 
-  val scalaCheck = "org.scalacheck" %% "scalacheck" % "1.11.4"
+  val scalaCheck = "org.scalacheck" %% "scalacheck" % "1.14.0"
 
-  val scalaTest = "org.scalatest" %% "scalatest" % "2.2.1"
+  val scalaTest = "org.scalatest" %% "scalatest" % "3.0.8"
 
-  val defaultAkkaVersion = "2.4.10"
-
-  def akkaModule(id: String, version: String = defaultAkkaVersion): ModuleID =
-    "com.typesafe.akka" %% s"akka-$id" % version
-
-  val akkaActor = akkaModule("actor") exclude ("com.typesafe", "config")
-  val akkaTestkit = akkaModule("testkit")
-
-  val sprayVersion = "1.3.3"
-  def sprayModule(id: String): ModuleID = "io.spray" %% s"spray-$id" % sprayVersion
-  val sprayRouting = sprayModule("routing")
-  val sprayClient = sprayModule("client")
-  val sprayTestkit = sprayModule("testkit")
-
-  // Spray json (separate from Spray toolkit)
-  val sprayJson = "io.spray" %% "spray-json" % "1.3.2"
+  val sprayJson = "io.spray" %% "spray-json" % "1.3.5"
 
   val typesafeConfig = "com.typesafe" % "config" % "1.2.1"
 
-  val scopt = "com.github.scopt" %% "scopt" % "3.3.0"
+  val scopt = "com.github.scopt" %% "scopt" % "3.7.1"
 
   object Logging {
-    val slf4jVersion = "1.7.10"
-    val logbackVersion = "1.1.2"
+    val slf4jVersion = "1.7.28"
+    val logbackVersion = "1.2.3"
     // The logging API to use. This should be the only logging dependency of any API artifact
     // (anything that's going to be depended on outside of this SBT project).
     val slf4jApi = "org.slf4j" % "slf4j-api" % slf4jVersion
     val logbackCore = "ch.qos.logback" % "logback-core" % logbackVersion
     val logbackClassic = "ch.qos.logback" % "logback-classic" % logbackVersion
+
+    val loggingDependencyOverrides = Seq(
+      Logging.slf4jApi,
+      Logging.logbackCore,
+      Logging.logbackClassic
+    )
   }
 
 }
