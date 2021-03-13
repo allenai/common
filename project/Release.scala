96fe9cccb1a62be34ca13563767ee270a81f1320
Brandon Stilson <bbstilson@fastmail.com>
2021-03-12 21:06:34 -0800

    override default release process

--- project/Release.scala
+++ project/Release.scala
@@ -1,9 +1,42 @@
+import ScalaVersions._
+
 import sbtrelease.ReleaseStateTransformations._
 import sbtrelease.ReleasePlugin.autoImport._
+import sbt._
+import sbt.Keys._
 
 object Release {
 
-  def settings: Seq[ReleaseStep] = Seq(
+  def settings: Seq[Setting[_]] = Seq(
+    organization := "org.allenai.common",
+    crossScalaVersions := SUPPORTED_SCALA_VERSIONS,
+    releaseProcess := releaseSteps,
+    unmanagedSourceDirectories.in(Compile) ++= {
+      CrossVersion.partialVersion(scalaVersion.value) match {
+        case Some((2, x)) if x == 11 || x == 12 =>
+          Seq(file(sourceDirectory.value.getPath + "/main/scala-2.11-2.12"))
+        case Some((2, x)) if x == 13 =>
+          Seq(file(sourceDirectory.value.getPath + "/main/scala-2.13"))
+        case _ => Seq.empty // dotty support would go here
+      }
+    },
+    publishArtifact in Test := false,
+    publishArtifact in (Compile, packageDoc) := false,
+    pomIncludeRepository := { _ =>
+      false
+    },
+    licenses += ("Apache-2.0", url("http://www.apache.org/licenses/LICENSE-2.0.html")),
+    homepage := Some(url("https://github.com/allenai/common")),
+    apiURL := Some(url("https://allenai.github.io/common/")),
+    scmInfo := Some(
+      ScmInfo(
+        url("https://github.com/allenai/common"),
+        "https://github.com/allenai/common.git"
+      )
+    )
+  )
+
+  def releaseSteps: Seq[ReleaseStep] = Seq(
     checkSnapshotDependencies,
     inquireVersions,
     runClean,
